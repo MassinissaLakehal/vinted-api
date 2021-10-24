f9b4ca5ec225794f742addd2be80eddc24ef05d7
Your Name <massii@hotmail.com>
2021-10-24 19:56:54 +0200

    init git

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
+const cors = require("cors");
 
 mongoose.connect("mongodb://localhost/vinted");
 
 cloudinary.config({
-  cloud_name: "dtmo9vwzh",
-  api_key: "549596979995117",
-  api_secret: "eNiyq7vRvUwrmXA5VKat0VLwSz0",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
+app.use(cors());
 app.use(formidable());
 
 const userRoutes = require("./routes/users");
